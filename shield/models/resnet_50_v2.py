75f466566b079b54f4f969418ad039fb4fc5a8d8
Nilaksh Das <nilakshdas@gatech.edu>
2018-11-17 20:44:02 -0500

    Added utils

---
+++ shield/models/resnet_50_v2.py
@@ -0,0 +1,108 @@
+import tensorflow as tf
+
+from cleverhans.model import Model as CleverHansModel
+
+from shield.slim.nets.resnet_v2 import resnet_arg_scope, resnet_v2_50
+
+slim = tf.contrib.slim
+
+
+def _get_updated_endpoints(original_end_points):
+    """Adds the keys 'logits' and 'probs' to the end points dictionary of ResNet50-v2.
+
+    Args:
+        original_end_points (dict): Original dictionary of end points
+
+    Returns:
+        dict: Dictionary of end points with the new keys.
+    """
+
+    end_points = dict(original_end_points)
+    end_points['logits'] = end_points['resnet_v2_50/spatial_squeeze']
+    end_points['probs'] = tf.nn.softmax(end_points['logits'])
+
+    return end_points
+
+
+class ResNet50v2(CleverHansModel):
+    """Wrapper class for the ResNet50-v2 model loaded from tensorflow slim.
+
+    Attributes:
+        x (tf.Variable): The variable in the tensorflow graph that feeds into the model nodes.
+        num_classes (int): Number of predicted classes for classification tasks.
+            If 0 or None, the features before the logit layer are returned.
+        default_image_size (int): The image size accepted by the model by default.
+        end_points (dict): Dictionary with pointers to intermediate variables of the model.
+        variables_to_restore (list): Names of tensorflow variables to be restored in the graph.
+    """
+
+    def __init__(self, x, num_classes=1001, is_training=False):
+        """Initializes the tensorflow graph for the ResNet50-v2 model.
+
+        Args:
+            x (tf.Variable): The variable in the tensorflow graph that feeds into the model nodes.
+            num_classes (int): Number of predicted classes for classification tasks.
+                If 0 or None, the features before the logit layer are returned.
+            is_training (bool): Whether batch_norm layers are in training mode.
+        """
+
+        super(ResNet50v2, self).__init__()
+
+        self.x = x
+        self.num_classes = num_classes
+        self.default_image_size = 299
+
+        # populating the tensorflow graph
+        with slim.arg_scope(resnet_arg_scope()):
+            net, end_points = resnet_v2_50(
+                x, num_classes=num_classes,
+                is_training=is_training, reuse=None)
+
+        self.end_points = _get_updated_endpoints(end_points)
+        self.variables_to_restore = slim.get_variables_to_restore(exclude=[])
+
+    def load_weights(self, checkpoint_path, sess=None):
+        """Load weights from a checkpoint file into the tensorflow graph.
+
+        Args:
+            checkpoint_path (str): Path to the checkpoint file.
+            sess (tf.Session): The tensorflow session holding the model graph.
+        """
+
+        if sess is None:
+            sess = tf.get_default_session()
+        assert sess is not None
+
+        saver = tf.train.Saver(self.variables_to_restore)
+        saver.restore(sess, checkpoint_path)
+
+    def get_params(self):
+        """Lists the model's parameters.
+
+        Returns:
+            list: A list of the model's parameters.
+        """
+
+        return None
+
+    def fprop(self, x):
+        """Exposes all the layers of the model.
+
+        Args:
+            x (tf.Variable): Tensor which is input to the model.
+
+        Returns:
+            dict: A dictionary mapping layer names to the corresponding
+                 node in the tensorflow graph.
+        """
+
+        if x is self.x:
+            return self.end_points
+
+        else:
+            with slim.arg_scope(resnet_arg_scope()):
+                net, end_points = resnet_v2_50(
+                    x, num_classes=self.num_classes,
+                    is_training=False, reuse=tf.AUTO_REUSE)
+
+            return _get_updated_endpoints(end_points)
---
+++ shield/utils/metering.py
@@ -0,0 +1,267 @@
+from threading import RLock as _Lock
+
+import numpy as np
+
+
+def _thread_safe(fn):
+    def wrapper(*args, **kwargs):
+        self = args[0]
+
+        with self._lock:
+            return_value = fn(*args, **kwargs)
+
+        return return_value
+
+    return wrapper
+
+
+class Meter(object):
+    """Base class for creating thread-safe meters.
+
+    Each meter tracks some parameters which are then
+    processed to evaluate the current meter value.
+
+    Parameter values can be iteratively offered
+    to the meter, and the meter uses the corresponding
+    private reducer methods to store the values
+    in the original form, or in some processed form.
+
+    The meter is thread-safe, i.e., it can be used in
+    parallel threads. The parameter values offered in
+    one call are reduced as an atomic unit.
+    This functionality is handled by this base class,
+    and the child classes need only implement the
+    abstract functions.
+    """
+
+    def __init__(self):
+        self._lock = _Lock()
+
+        self._parameters = {}
+        self._reducers = {}
+
+    def _add_parameter(self, name, initial_value, reducer):
+        self._parameters[name] = initial_value
+        self._reducers[name] = reducer
+
+    @_thread_safe
+    def _update_parameters(self, **kwargs):
+        for name, new_value in kwargs.items():
+            assert name in self._parameters
+
+            current_value = self._parameters[name]
+            reducer = self._reducers[name]
+
+            self._parameters[name] = \
+                reducer(current_value, new_value)
+
+    @_thread_safe
+    def _get_parameter(self, name):
+        return self._parameters[name]
+
+    @_thread_safe
+    def _get_parameters(self, names):
+        return tuple([self._get_parameter(name) for name in names])
+
+    def offer(self, **kwargs):
+        """Abstract function that accepts parameter values
+        to be processed and saved by the meter."""
+
+        raise NotImplementedError
+
+    def evaluate(self):
+        """Abstract function that processes the parameters
+        and returns the current meter value."""
+
+        raise NotImplementedError
+
+    def save(self, npz_save_path):
+        """Saves the meter parameters to a .npz file.
+
+        Args:
+            npz_save_path: Path to .npz file
+                where parameters will be saved.
+        """
+
+        with open(npz_save_path, 'w') as npzfile:
+            np.savez(npzfile, **self._parameters)
+
+    def load(self, npz_load_path):
+        """Loads meter parameters from a .npz file.
+
+        Args:
+            npz_load_path: Path to .npz file
+                where parameters will be loaded from.
+
+        Returns:
+            The meter value after loading the data.
+        """
+
+        npzfile = np.load(npz_load_path)
+        self._update_parameters(**npzfile)
+
+        return self.evaluate()
+
+
+class AccuracyMeter(Meter):
+    """A meter to track accuracy of the model."""
+
+    def __init__(self):
+        """Initializes the meter.
+
+        This meter tracks the following parameters:
+            - ids
+            - y_pred
+            - y_true
+        """
+        super(AccuracyMeter, self).__init__()
+
+        arr_reducer = lambda a, b: np.append(a, b)
+
+        self._add_parameter('ids', np.zeros((0,), dtype=str), arr_reducer)
+        self._add_parameter('y_pred', np.zeros((0,)), arr_reducer)
+        self._add_parameter('y_true', np.zeros((0,)), arr_reducer)
+
+    def offer(self, y_pred, y_true, ids=None):
+        """Tracks the corresponding parameter values.
+
+        Args:
+            y_pred (np.ndarray): A 1-D list of predicted class labels.
+            y_true (np.ndarray): A 1-D list of true class labels.
+            ids (np.ndarray, optional):  A 1-D list of instance ID's.
+        """
+
+        y_pred, y_true = np.array(y_pred), np.array(y_true)
+        assert y_pred.shape[0] == y_true.shape[0]
+        n = y_true.shape[0]
+
+        ids = (ids if ids is not None
+               else np.zeros((n,), dtype=str))
+
+        self._update_parameters(
+            ids=ids, y_true=y_true, y_pred=y_pred)
+
+    def evaluate(self):
+        """Calculates and returns the current accuracy tracked by the meter.
+
+        Returns:
+            float: Current accuracy.
+        """
+
+        y_pred, y_true = self._get_parameters(['y_pred', 'y_true'])
+        correct, total = len(np.where(y_true == y_pred)[0]), y_true.shape[0]
+        return correct / float(total)
+
+
+class TopKAccuracyMeter(Meter):
+    """A meter to track top-k accuracy of the model."""
+
+    def __init__(self):
+        """Initializes the meter.
+
+        This meter tracks the following parameters:
+            - ids
+            - top_k_preds
+            - y_true
+        """
+
+        super(TopKAccuracyMeter, self).__init__()
+
+        arr_reducer = lambda a, b: np.append(a, b, axis=0)
+
+        self._add_parameter('ids', np.zeros((0,), dtype=str), arr_reducer)
+        self._add_parameter('top_k_preds', np.zeros((0, 5)), arr_reducer)
+        self._add_parameter('y_true', np.zeros((0,)), arr_reducer)
+
+    def offer(self, top_k_preds, y_true, ids=None):
+        """Tracks the corresponding parameter values.
+
+        Args:
+            top_k_preds (np.ndarray): A matrix of shape (n, k)
+                of predicted class labels.
+            y_true (np.ndarray): A 1-D list of true class labels.
+            ids (np.ndarray, optional):  A 1-D list of instance ID's.
+        """
+
+        y_true = np.array(y_true)
+        assert top_k_preds.shape[0] == y_true.shape[0]
+        n = y_true.shape[0]
+
+        ids = (ids if ids is not None
+               else np.zeros((n,), dtype=str))
+
+        self._update_parameters(
+            ids=ids, top_k_preds=top_k_preds, y_true=y_true)
+
+    def evaluate(self):
+        """Calculates and returns the current top-k accuracy
+        tracked by the meter.
+
+        Returns:
+            float: Current top-k accuracy.
+        """
+
+        top_k_preds, y_true = self._get_parameters(['top_k_preds', 'y_true'])
+        correct, total = \
+            len(np.where(y_true.reshape(-1, 1) == top_k_preds)[0]), \
+            y_true.shape[0]
+        return correct / float(total)
+
+
+class AverageNormalizedL2DistanceMeter(Meter):
+    """A meter to track the average normalized L2 distance
+    between original and perturbed images."""
+
+    def __init__(self):
+        """Initializes the meter.
+
+        This meter tracks the following parameters:
+            - original_L2
+            - perturbation_L2
+        """
+
+        super(AverageNormalizedL2DistanceMeter, self).__init__()
+
+        arr_reducer = lambda a, b: np.append(a, b)
+
+        self._add_parameter(
+            'original_L2', np.zeros((0,), dtype=np.float32), arr_reducer)
+        self._add_parameter(
+            'perturbation_L2', np.zeros((0,), dtype=np.float32), arr_reducer)
+
+    def offer(self, original, perturbed):
+        """Tracks the corresponding parameter values.
+
+        Args:
+            original (np.ndarray): A matrix of shape (n, d1, d2, ..., dm)
+                for the original batch of images.
+            perturbed (np.ndarray): A matrix of shape (n, d1, d2, ..., dm)
+                for the perturbed batch of images.
+        """
+
+        assert perturbed.shape == original.shape
+
+        n = original.shape[0]
+        original_, perturbed_ = original.reshape((n, -1,)), \
+                                perturbed.reshape((n, -1,))
+
+        self._update_parameters(
+            original_L2=np.linalg.norm(
+                original_,
+                ord=2, axis=1),
+            perturbation_L2=np.linalg.norm(
+                perturbed_ - original_,
+                ord=2, axis=1))
+
+    def evaluate(self):
+        """Calculates and returns the current average normalized L2 distance
+        tracked by the meter.
+
+        Returns:
+            float: Current average normalized L2 distance.
+        """
+
+        original_L2, perturbation_L2 = \
+            self._get_parameters(['original_L2', 'perturbation_L2'])
+
+        return np.mean(perturbation_L2 / original_L2)
